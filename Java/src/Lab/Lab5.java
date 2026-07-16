d67332adf544c8b9582e8bdef79519cce266f7b1
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-17 00:32:12 +0700

    Lab5

--- Java/src/Lab/Lab5.java
+++ Java/src/Lab/Lab5.java
@@ -1,7 +1,36 @@
 package Lab;
 
-public class Lab5 {
-    public static void main(String[] args) {
+import utils.Validation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lab5 {
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+       List<Double> nums = new ArrayList<>();
+       double sum = 0;
+
+        while(true){
+            System.out.print("Mời bạn nhập 1 số bất kỳ: ");
+            nums.add(Validation.validateOrDefault(sc.nextDouble(),false));
+            sc.nextLine();
+            if(!confirm(sc)) break;
+            System.out.println("Mời bạn tiếp tục!");
+        }
+        for (double num : nums){
+            System.out.print(num+" ");
+            sum+=num;
+        }
+        System.out.format("\n Tổng của mảng %s = %.1f",nums.toString(),sum);
+    }
+    private static boolean confirm(Scanner sc) {
+        System.out.print("Nhập thêm Y/N? ");
+        String choice = sc.nextLine().trim();
+        if (choice.equalsIgnoreCase("Y")) return true;
+        if (choice.equalsIgnoreCase("N")) return false;
+        System.out.println("Bạn đã nhập sai lựa chọn, vui lòng nhập lại!");
+        return confirm(sc);
     }
 }
